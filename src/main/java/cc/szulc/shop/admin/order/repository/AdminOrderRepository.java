d28b193849c0339d1ce241230e9d78680c1032e3
Jacek Szulc <jacek@szulc.cc>
2023-03-28 20:50:52 +0200

    Export of orders

--- src/main/java/cc/szulc/shop/admin/order/repository/AdminOrderRepository.java
+++ src/main/java/cc/szulc/shop/admin/order/repository/AdminOrderRepository.java
@@ -1,7 +1,12 @@
 package cc.szulc.shop.admin.order.repository;
 
 import cc.szulc.shop.admin.order.model.AdminOrder;
+import cc.szulc.shop.admin.order.model.AdminOrderStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface AdminOrderRepository extends JpaRepository<AdminOrder, Long> {
+    List<AdminOrder> findAllByPlaceDateIsBetweenAndOrderStatus(LocalDateTime from, LocalDateTime to, AdminOrderStatus orderStatus);
 }
---
+++ src/main/java/cc/szulc/shop/admin/order/service/AdminExportService.java
@@ -0,0 +1,23 @@
+package cc.szulc.shop.admin.order.service;
+
+import cc.szulc.shop.admin.order.model.AdminOrder;
+import cc.szulc.shop.admin.order.model.AdminOrderStatus;
+import cc.szulc.shop.admin.order.repository.AdminOrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class AdminExportService {
+
+    private final AdminOrderRepository orderRepository;
+
+    public List<AdminOrder> exportOrders(LocalDateTime from, LocalDateTime to, AdminOrderStatus orderStatus) {
+        return orderRepository.findAllByPlaceDateIsBetweenAndOrderStatus(from, to, orderStatus);
+    }
+}
+
