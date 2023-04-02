7d5d1e3d7630596f8fa2909376c6747125589f1d
Jacek Szulc <jacek@szulc.cc>
2023-04-02 15:22:08 +0200

    Forgotten password recovery.

---
+++ src/main/java/cc/szulc/shop/security/LostPasswordController.java
@@ -0,0 +1,28 @@
+package cc.szulc.shop.security;
+
+import cc.szulc.shop.security.model.EmailObject;
+import cc.szulc.shop.security.model.dto.ChangePassword;
+import cc.szulc.shop.security.service.LostPasswordService;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class LostPasswordController {
+
+    private final LostPasswordService lostPasswordService;
+
+    @PostMapping("/lostPassword")
+    public void lostPassword(@RequestBody EmailObject emailObject) {
+        lostPasswordService.sendLostPasswordLink(emailObject);
+    }
+
+    @PostMapping("/changePassword")
+    public void changePassword(@RequestBody ChangePassword changePassword) {
+        lostPasswordService.changePassword(changePassword);
+    }
+}
